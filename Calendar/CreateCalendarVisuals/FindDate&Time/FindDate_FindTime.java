8125cf52987c6a8719bb4c7b7020e15dec93a4ef
Ian <13.ianma@gmail.com>
2021-04-20 09:39:48 -0500

    Added a variable to store time and printed it.

--- Calendar/CreateCalendarVisuals/FindDate&Time/FindDate_FindTime.java
+++ Calendar/CreateCalendarVisuals/FindDate&Time/FindDate_FindTime.java
@@ -1,11 +1,13 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class FindDate_FindTime
 {
 public static void main(String[] args)
 {
         LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
 
-        System.out.println(date);
+        System.out.println("Date: " + date + "; Time: " + time);
 }
 }
