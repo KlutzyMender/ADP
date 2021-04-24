33b673a7d9c61c2f7a4ff6827ea7e62c9ecd5f8f
Ian <13.ianma@gmail.com>
2021-04-24 12:30:46 -0500

    Update FindDate_FindTime.java
    Added a method to find day of the week

--- Calendar/CreateCalendarVisuals/FindDate&Time/FindDate_FindTime.java
+++ Calendar/CreateCalendarVisuals/FindDate&Time/FindDate_FindTime.java
@@ -1,4 +1,7 @@
+package A;
+
 import java.time.LocalDate;
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class FindDate_FindTime
@@ -9,5 +12,13 @@ public static void main(String[] args)
         LocalTime time = LocalTime.now();
 
         System.out.println("Date: " + date + "; Time: " + time);
+}
+
+public static int dayOfWeek(int year, int month, int day)
+{
+        LocalDate date = LocalDate.of(year, month, day);
+
+        DayOfWeek d = date.getDayOfWeek();
+        return d.getValue();
 }
 }
