ec70905303772232f539edb5c578ca1b25ecbf5d
Ian <13.ianma@gmail.com>
2021-04-27 20:55:41 -0500

    Creating more functions to then create the visauls for the calendar

--- Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
+++ Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
@@ -5,6 +5,12 @@ public class CreateCalendarVisuals
 public static void main(String[] args)
 {
         FindDateFindTime d = new FindDateFindTime();
-        System.out.println(d.dayOfWeek(2019, 04, 27));
+
+        System.out.println("Year: " + d.currentYear());
+        System.out.println("Month: " + d.currentMonth());
+        System.out.println("Day: " + d.currentDay());
+
+        System.out.print("test: " + d.lengthOfAllMonthsInYear(2021)[0]);
+
 }
 }
--- Calendar/CreateCalendarVisuals/FindDateFindTime/FindDateFindTime.java
+++ Calendar/CreateCalendarVisuals/FindDateFindTime/FindDateFindTime.java
@@ -1,24 +1,66 @@
-package finddatefindtime;
+package finddatefindtime; //creats a package (definds the name of the foler this file is in)
 
-import java.time.LocalDate;
-import java.time.DayOfWeek;
-import java.time.LocalTime;
+//imports:
+import java.util.ArrayList;
 
+import java.time.LocalDate; //import local date to find date
+import java.time.DayOfWeek; // import day of week to be able to find the day of the week
+import java.time.LocalTime; // import local time to find time
+
+//create a class called FindDateFindTime
 public class FindDateFindTime
 {
-public static void main(String[] args)
-{
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.now();
+  //store the local date class on the variable date
+  LocalDate date = LocalDate.now(); // .now finds current date
 
-        System.out.println("Date: " + date + "; Time: " + time);
-}
+  //class to find the currentYear
+  public int currentYear()
+  {
+    return date.getYear(); // returns the curent year
+  }
 
-public int dayOfWeek(int year, int month, int day)
-{
-        LocalDate date = LocalDate.of(year, month, day);
+  //class to find the currentMonth
+  public int currentMonth()
+  {
+    return date.getMonthValue(); // returns the curent month
+  }
 
-        DayOfWeek d = date.getDayOfWeek();
-        return d.getValue();
-}
+  //class to find the currentDay
+  public int currentDay()
+  {
+    return date.getDayOfMonth(); // returns the curent Day
+  }
+
+  public int[] lengthOfAllMonthsInYear(int year)
+  {
+    //creat an array to store each lenght of each month
+    int[] lengthOfMonthsArray = new int[12];
+    //starts a loop to find the lenght of each month for the given year
+    for(int i = 1; i < 13; i++)
+    {
+      //adds the months lenght to the array
+      lengthOfMonthsArray[i-1] = lengthOfMonth(year, i);
+    }
+
+    return lengthOfMonthsArray; // returns the lengthOfallMonths
+  }
+
+  public int lengthOfMonth(int year, int month)
+  {
+    //store the local date class on the variable date
+    LocalDate dateof = LocalDate.of(year, month, 1);
+
+    return dateof.lengthOfMonth();
+  }
+  // creats a function to find the day of the week for any given day of all time
+  // the user will input a date to then find the day of the week that lands on
+  public int dayOfWeek(int year, int month, int day)
+  {
+    //store the local date class on the variable date
+    LocalDate dateof = LocalDate.of(year, month, day); // uses the user input to find day of week
+
+    // gets the day of week with the inputed year, month and day
+    DayOfWeek dow = dateof.getDayOfWeek();
+    return dow.getValue(); // return the day of the week
+  }
 }
