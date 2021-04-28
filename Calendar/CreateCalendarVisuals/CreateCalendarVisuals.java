673e93a3f2e2a2ce593ed24e45a98cfed02fda4f
Ian <13.ianma@gmail.com>
2021-04-28 08:43:57 -0500

    Added a class to find the day of week at the start and end of each month

--- Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
+++ Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
@@ -10,7 +10,9 @@ public static void main(String[] args)
         System.out.println("Month: " + d.currentMonth());
         System.out.println("Day: " + d.currentDay());
 
-        System.out.print("test: " + d.lengthOfAllMonthsInYear(2021)[0]);
+        System.out.println("test: " + d.lengthOfMonth(2021, 1));
+        System.out.println("test: " + d.dayOfWeek(2021, 1, 31));
+        System.out.println("test: " + d.dayOfWeekForStartAndEndOfMonths(2021)[1]);
 
 }
 }
--- Calendar/CreateCalendarVisuals/FindDateFindTime/FindDateFindTime.java
+++ Calendar/CreateCalendarVisuals/FindDateFindTime/FindDateFindTime.java
@@ -31,8 +31,34 @@ public class FindDateFindTime
     return date.getDayOfMonth(); // returns the curent Day
   }
 
-  public int[] lengthOfAllMonthsInYear(int year)
+  public int[] dayOfWeekForStartAndEndOfMonths(int year)
   {
+    //creat an array to store each lenght of each month
+    int[] lengthOfMonthsArray = lengthOfAllMonthsInYear(year);
+    //creat an array to store each day of the week for the satrt and end of each month
+    int[] dayOfWeekForStartAndEndOfMonthsArray = new int[24];
+
+    // loops throght all the months
+    // Adds the day of the week for the start and end of each month to the variable
+    for(int i = 0; i < 24; i++)
+    {
+      int n = 1; // variable to have the month
+      if(i % 2 == 0) // every other loop find the first day of month
+      {
+        dayOfWeekForStartAndEndOfMonthsArray[i] = dayOfWeek(year, n, 1);
+      }
+      else // every other loop find the last day of month
+      {
+        dayOfWeekForStartAndEndOfMonthsArray[i] = dayOfWeek(year, n, lengthOfMonth(year, n));
+        n++;
+      }
+    }
+    //returns the array with all the dayOfWeekForStartAndEndOfMonths
+    return dayOfWeekForStartAndEndOfMonthsArray;
+  }
+
+    public int[] lengthOfAllMonthsInYear(int year)
+    {
     //creat an array to store each lenght of each month
     int[] lengthOfMonthsArray = new int[12];
     //starts a loop to find the lenght of each month for the given year
@@ -45,17 +71,17 @@ public class FindDateFindTime
     return lengthOfMonthsArray; // returns the lengthOfallMonths
   }
 
-  public int lengthOfMonth(int year, int month)
-  {
+      public int lengthOfMonth(int year, int month)
+      {
     //store the local date class on the variable date
     LocalDate dateof = LocalDate.of(year, month, 1);
 
     return dateof.lengthOfMonth();
   }
-  // creats a function to find the day of the week for any given day of all time
-  // the user will input a date to then find the day of the week that lands on
-  public int dayOfWeek(int year, int month, int day)
-  {
+    // creats a function to find the day of the week for any given day of all time
+    // the user will input a date to then find the day of the week that lands on
+    public int dayOfWeek(int year, int month, int day)
+    {
     //store the local date class on the variable date
     LocalDate dateof = LocalDate.of(year, month, day); // uses the user input to find day of week
 
