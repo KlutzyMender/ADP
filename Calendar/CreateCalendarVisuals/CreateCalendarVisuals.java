aac66a8c23607945658959ac249d0a3fdd4d58d0
Ian <13.ianma@gmail.com>
2021-05-13 15:44:57 -0500

    Added functionality to cahnge the months and move bettwen the years

--- Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
+++ Calendar/CreateCalendarVisuals/CreateCalendarVisuals.java
@@ -15,6 +15,7 @@ public class CreateCalendarVisuals extends Canvas implements MouseListener
   FindDateFindTime d = new FindDateFindTime(); // starts my FindDateFindTime class
 
   public int calendarDisplayedMonth = d.currentMonth();
+  public int calendarDisplayedYear = d.currentYear();
 
   public final int calendarStarting_X_Location = 100;
   public final int calendarStarting_Y_Location = 100;
@@ -47,18 +48,13 @@ public class CreateCalendarVisuals extends Canvas implements MouseListener
   public void paint(Graphics g)
   {
     // FindDateFindTime class...
-    makeCalendar(calendarDisplayedMonth, g);
+    makeCalendar(calendarDisplayedMonth, calendarDisplayedYear, g);
     makeButton(0,0,100,50,Color.RED, g);
     makeButton(100,0,100,50,Color.BLUE, g);
-
-    for(int i = 0; i < xArray.size(); i++)
-    {
-      System.out.println(xArray.get(i));
-    }
   } // end of paint finction
 
   // function to draw a box for the date
-  public void makeCalendar(int month, Graphics g)
+  public void makeCalendar(int month, int year, Graphics g)
   {
     // variables for the boxes...
     int x = calendarStarting_X_Location; // creats the variable to store the x location of the day boxes on the calendar
@@ -71,7 +67,7 @@ public class CreateCalendarVisuals extends Canvas implements MouseListener
 
     //for(int i = 1; i <= 12; i++)
     //{
-      int startOfMonth = d.dayOfWeek(2021, month, 1); // find the start of the month
+      int startOfMonth = d.dayOfWeek(year, month, 1); // find the start of the month
       int dayofmonth = 1; // variable to store the day of the month to be printed
 
       //prints 0 for all the days leading up to the first of the month
@@ -103,7 +99,7 @@ public class CreateCalendarVisuals extends Canvas implements MouseListener
         {
           // if the day of month is bigger then the number of days in a month
           // then print 00
-          if(dayofmonth > d.lengthOfMonth(2021, month))
+          if(dayofmonth > d.lengthOfMonth(year, month))
           {
             //prints 00 to represent the days that are not in that month
             drawDateRect(x, y, width, height, 0, Color.GRAY,  g);
@@ -176,42 +172,69 @@ public class CreateCalendarVisuals extends Canvas implements MouseListener
     g.setColor(boxColor); // changes the color
     // creates a rect to show the date
     g.fillRect(x, y, width, height); // starts at location x and y and has a width of width
-  }
+  } // end of makeButton class
 
-
+  // this function will run everytime the mouse is clicked
   public void mouseClicked(MouseEvent event)
   {
-    Graphics g = getGraphics();
+    Graphics g = getGraphics(); // gets the Graphics class to be able to draw
 
+    //loops through all the buttons that we have made
     for(int i = 0; i < xArray.size(); i++)
     {
-      double mouseX = MouseInfo.getPointerInfo().getLocation().x;
-      double mouseY = MouseInfo.getPointerInfo().getLocation().y;
+      double mouseX = MouseInfo.getPointerInfo().getLocation().x; // makes a variable to store the mouse x location
+      double mouseY = MouseInfo.getPointerInfo().getLocation().y; // makes a variable to store the mouse y location
 
-      // System.out.println("mouse x: " + mouseX);
-      // System.out.println("mouse y: " + mouseY);
-
+      // if the mouse is within the x position of the button
       if(mouseX >= xArray.get(i) && mouseX <= xArray.get(i) + widthArray.get(i))
       {
-        if(mouseY >= yArray.get(i) && mouseY <= yArray.get(i) + heightArray.get(i))
+        // if the mouse is within the y position of the button
+        if(mouseY >= yArray.get(i) && mouseY <= yArray.get(i) + 2*heightArray.get(i))
         {
-          g.clearRect(0, 100, canvas.getWidth(), canvas.getHeight());
+          // clear the screen below y = 50
+          g.clearRect(0, 50, canvas.getWidth(), canvas.getHeight());
+          // if button 1
           if(i == 0)
           {
-            makeCalendar(calendarDisplayedMonth -= 1, g);
-            break;
+            calendarDisplayedMonth -= 1;
+
+            if(calendarDisplayedMonth <= 0)
+            {
+              calendarDisplayedMonth = 12;
+              calendarDisplayedYear -= 1;
+            }
+            System.out.println(calendarDisplayedMonth);
+            // remakes the calendar but one month back
+            makeCalendar(calendarDisplayedMonth, calendarDisplayedYear, g);
+            break; // stops the loop
           }
+          // if button 2
           if(i == 1)
           {
-            makeCalendar(calendarDisplayedMonth += 1, g);
-            break;
+            calendarDisplayedMonth += 1;
+
+            if(calendarDisplayedMonth >= 13)
+            {
+              calendarDisplayedMonth = 1;
+              calendarDisplayedYear += 1;
+            }
+
+            System.out.println(calendarDisplayedMonth);
+            // remakes the calendar but one month forward
+            makeCalendar(calendarDisplayedMonth, calendarDisplayedYear, g);
+            break; // stops the loop
           }
         }
       }
     }
   }
+  // i only need the mouseClicked function but you have to put them all in inorder to work
+  // this function will run everytime the mouse is pressed
   public void mousePressed(MouseEvent e){}
+  // this function will run everytime the mouse has entered
   public void mouseEntered(MouseEvent e){}
+  // this function will run everytime the mouse has exited
   public void mouseExited(MouseEvent e){}
+    // this function will run everytime the mouse is released
   public void mouseReleased(MouseEvent e){}
 } // end of class
